7ee8775a68274607a815bd6954646d83e327b418
Viviana218 <104119912+Viviana218@users.noreply.github.com>
2022-10-12 17:46:32 -0500

    inicializando

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import PersonCardFuncional from "./Componentes/PersonCardFuncional/PersonCardFuncional";
+
+/*  Inicializar: npm start
+Detener proyecto: Ctrl+C 
+*/
+
+class App extends React.Component {
+
+  
+
+  render() {
+    return(
+      <div className="container">
+        <br/>
+        <PersonCardFuncional firstName="Jane" lastName="Doe" age={45} hairColor="Black"/>
+        <br/>
+        <PersonCardFuncional firstName="John" lastName="Smith" age={88} hairColor="Brown"/>
+        <br/>
+        <PersonCardFuncional firstName="Millard" lastName="Fillmore" age={50} hairColor="Brown"/>
+        <br/>
+        <PersonCardFuncional firstName="Maria" lastName="Smith" age={62} hairColor="Brown"/>
+
+
+      </div>
+      
+    );
+  }
+
+}
+
+export default App;
